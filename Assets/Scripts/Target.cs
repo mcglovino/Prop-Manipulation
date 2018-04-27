066f5d837de3843ca211a80a62f5f0e3bf044fe5
OVERWATCHER\Overwatcher <paoloftuna@googmail.com>
2018-04-27 16:31:51 +0100

    +Will not respawn outside of area

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -29,22 +29,29 @@ public class Target : MonoBehaviour {
         else if (VectorMaths.Distance(GetComponent<TRS>().position, Goat.GetComponent<TRS>().position) < 2.5f)
         {
             GetComponent<MeshRenderer>().material = Orange;
+            GoAgain.SetActive(false);
         }
         else
         {
             GetComponent<MeshRenderer>().material = Red;
+            GoAgain.SetActive(false);
+        }
+
+        if (GetComponent<TRS>().position.x > 10 || GetComponent<TRS>().position.x < -10
+            || GetComponent<TRS>().position.y > 7 || GetComponent<TRS>().position.y < -7
+            || GetComponent<TRS>().position.z > 10 || GetComponent<TRS>().position.z < -2)
+        {
+            Relocate();
         }
     }
 
     public void Relocate()
     {
-        GetComponent<LerpTo>().targetPos = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-7.0f, 7.0f), Random.Range(-2.0f, 10.0f));
-
-        GetComponent<LerpTo>().targetRot = new Vector3(Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f));
-
-        GoAgain.SetActive(false);
+        //GetComponent<LerpTo>().targetPos = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-7.0f, 7.0f), Random.Range(-2.0f, 10.0f));
+        GetComponent<LerpTo>().targetPos = VectorMaths.Add(Goat.GetComponent<TRS>().position, VectorMaths.Normalized(VectorMaths.EulertoDir(new Vector3(Random.Range(0, Mathf.PI * 2), Random.Range(0, Mathf.PI * 2), Random.Range(0, Mathf.PI * 2)))) * 6);
+        //GetComponent<LerpTo>().targetRot = new Vector3(Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f));
         GetComponent<MeshRenderer>().material = Red;
+        GoAgain.SetActive(false);
 
-        GetComponent<LerpTo>().targetPos = VectorMaths.Add(Goat.GetComponent<TRS>().position, VectorMaths.Normalized(VectorMaths.EulertoDir(new Vector3(Mathf.PI*2, Mathf.PI * 2, Mathf.PI * 2))) * 6);
     }
 }
